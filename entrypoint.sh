d0b7455e1de5bf118f798f8ca00c72bd8d1e63cb
Robert Vilvert <robert.vilvert@resultadosdigitais.com.br>
2018-03-08 13:38:05 -0300

    Entrypoint improve

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 sed -i  "s/LOGENTRIESTOKEN/$TOKEN/g" /etc/td-agent/logentries-tokens.conf
-echo $CA_CERT >> /etc/td-agent/secret_ca_file
+echo -e "$CA_CERT" >> /etc/td-agent/secret_ca_file
 
 exec /usr/sbin/td-agent
